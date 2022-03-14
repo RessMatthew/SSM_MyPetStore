fbaa9860ec25cb780cb1390aedb8987f64b5ce46
XieWantong <8204203014@csu.edu.cn>
2022-03-14 18:19:46 +0800

    CSS小调整

--- src/main/java/org/csu/mypetstore/domain/Order.java
+++ src/main/java/org/csu/mypetstore/domain/Order.java
@@ -16,34 +16,37 @@ public class Order implements Serializable {
     private int orderId;
     private String username;
     private Date orderDate;
-
     private String shipAddress1;
     private String shipAddress2;
+
     private String shipCity;
     private String shipState;
     private String shipZip;
     private String shipCountry;
-    private String shipToFirstName;
-    private String shipToLastName;
-
     private String billAddress1;
+
     private String billAddress2;
     private String billCity;
     private String billState;
     private String billZip;
     private String billCountry;
-    private String billToFirstName;
-    private String billToLastName;
 
     private String courier;
     private BigDecimal totalPrice;
+    private String billToFirstName;
+    private String billToLastName;
+    private String shipToFirstName;
+
+    private String shipToLastName;
     private String creditCard;
     private String expiryDate;
     private String cardType;
     private String locale;
+
     private String status;
 
     private List<LineItem> lineItems = new ArrayList<LineItem>();
+
 
     public int getOrderId() {
         return orderId;
