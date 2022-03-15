34eb562516f4408f4f6240369b734b3855eea3f8
Aier233 <93068627+Aier233@users.noreply.github.com>
2022-03-15 10:40:12 +0800

    新增邮件验证

--- src/main/java/org/csu/mypetstore/controller/AccountController.java
+++ src/main/java/org/csu/mypetstore/controller/AccountController.java
@@ -3,6 +3,9 @@ package org.csu.mypetstore.controller;
 import org.csu.mypetstore.domain.User;
 import org.csu.mypetstore.service.UserService;
 import org.csu.mypetstore.util.AuthCodeUtil;
+import org.csu.mypetstore.util.JavaMailUtil;
+import org.csu.mypetstore.util.RandomNumberUtil;
+import org.csu.mypetstore.util.htmlText;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.imageio.ImageIO;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -18,6 +25,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Properties;
 
 @Controller
 @RequestMapping("/account")
@@ -213,6 +221,45 @@ public class AccountController {
         out.flush();
         out.close();
 
+
+    }
+
+    @PostMapping("/sendVCode")
+    public void sendVCode(HttpServletRequest request){
+
+        String reminder = null;
+        String vCode = null;
+        String email = null;
+        try{
+            //获取email的值
+            email = request.getParameter("email");
+            JavaMailUtil.receiveMailAccount = email;
+
+            Properties pops = new Properties();
+            pops.setProperty("mail.debug","true");
+            pops.setProperty("mail.smtp.auth","true");
+            pops.setProperty("mail.host",JavaMailUtil.emailSMTPHost);
+            pops.setProperty("mail.transport.protocol","smtp");
+            Session session = Session.getInstance(pops);
+            session.setDebug(true);
+            vCode = RandomNumberUtil.getRandomNumber();
+            System.out.println("邮箱验证码" + vCode);
+            String html = htmlText.html(vCode);
+            MimeMessage message = JavaMailUtil.creatMimeMessage(session, JavaMailUtil.emailAccount,
+                    JavaMailUtil.receiveMailAccount,html);
+            Transport transport = session.getTransport();
+            transport.connect(JavaMailUtil.emailAccount,JavaMailUtil.emailPassword);
+            transport.sendMessage(message,message.getAllRecipients());
+            transport.close();
+
+            reminder = "验证码发送成功";
+            request.setAttribute("reminder",reminder);
+            request.getSession().setAttribute("vCode",vCode);
+        }
+        catch (MessagingException | IOException m){
+            m.printStackTrace();
+            request.getSession().setAttribute("error","邮件发送失败");
+        }
 
     }
 
--- src/main/resources/static/js/mail.js
+++ src/main/resources/static/js/mail.js
@@ -1,20 +1,19 @@
-// $(function () {
-//     $("#button").click(function () {
-//         if ($("#email").val()) {
-//             $.ajax({
-//                 type: "POST",
-//                 url: "SendVCodeServlet",
-//                 data: {
-//                     email: $("#email").val(),
-//                 },
-//                 success: function (data) {
-//                     alert("验证码发送成功，请注意查收。");
-//                 },
-//             })
-//         } else {
-//             alert("邮箱发送失败");
-//         }
-//     });
-// });
+$(function () {
+    $("#SendVCode").click(function () {
+        if ($("#email").val()) {
+            $.ajax({
+                type: "POST",
+                url: "/account/sendVCode",
+                data: {
+                    email: $("#email").val(),
+                },
+                success: function (data) {
+                    alert("验证码发送成功，请注意查收。");
+                },
+            })
+        } else {
+            alert("邮箱发送失败");
+        }
+    });
+});
 
-//asdasdasdasdadasdasdasd
